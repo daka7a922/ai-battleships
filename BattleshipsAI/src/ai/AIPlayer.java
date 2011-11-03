d01e3f16a89cf6492ee4ad2b5d718c7282a3ead3
jfrankenbach <jfrankenbach@0bc47b83-1cd6-cde2-627f-47da32fcdf07>
2011-11-03 08:38:07 +0000

    changed AIPlayer again to random

--- BattleshipsAI/src/ai/AIPlayer.java
+++ BattleshipsAI/src/ai/AIPlayer.java
@@ -326,35 +326,8 @@ public class AIPlayer extends AbstractPlayer {
 	
 	protected int[] choseField(List<String> list) {
 		int[] result = new int[2];
-		HashMap<String, Integer> fields = new HashMap<String, Integer>();
-		for(String s : list) {
-			System.out.println(s);
-			String[] array = s.split(" ");
-			for(String a : array) {
-				if(!fields.keySet().contains(a)) {
-					fields.put(a, 1);
-				} else {
-					int x = fields.get(a);
-					fields.put(a, x + 1);
-				}
-			}
-		}
-		List<String> fieldList = new ArrayList<String>();
-		while(fields.keySet().size() > 0) {
-			int max = 0;
-			String field = "";
-			for(String str : fields.keySet()) {
-				if(fields.get(str) > max) {
-					max = fields.get(str);
-					field = str;
-				}
-			}
-			fields.remove(field);
-			fieldList.add(field);
-		}
-		System.out.println();
-		result[0] = Integer.parseInt(fieldList.get(0).substring(1, 2));
-		result[1] = Integer.parseInt(fieldList.get(0).substring(2, 3));
+		result[0] = (int)(Math.random() * 10);
+		result[1] = (int)(Math.random() * 10);
 		return result;
 	}
 }
